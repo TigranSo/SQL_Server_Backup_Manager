23e2f5a21a11b11631e56bcfe26825de5dad4f8d
Tigran <129489371+TigranSo@users.noreply.github.com>
2025-12-08 13:22:09 +0500

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DEFAULT_BACKUP_PATH = os.getenv(r'DEFAULT_BACKUP_PATH', r'localhost')
+DEFAULT_BACKUP_PATH = os.getenv(r'DEFAULT_BACKUP_PATH', r'G:\backup')
 
 SETTINGS_FILE = os.getenv('SETTINGS_FILE', 'connection_history.json')
 
@@ -13,4 +13,3 @@ DEFAULT_USER = os.getenv('DEFAULT_USER', 'sa')
 
 # Интервал проверки планировщика (в миллисекундах)
 SCHEDULER_CHECK_INTERVAL = int(os.getenv('SCHEDULER_CHECK_INTERVAL', '30000'))
-
